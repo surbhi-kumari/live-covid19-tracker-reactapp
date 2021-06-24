472a6d77b40225e7f383070d11ba3613513a2cb4
surbhi-kumari <64743248+surbhi-kumari@users.noreply.github.com>
2021-06-24 23:18:23 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CovidTracker from "./CovidTracker";
+
+export default function App() {
+  return (
+    <div className="App">
+      <CovidTracker />
+    </div>
+  );
+}
---
+++ src/CovidTracker.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+
+const CovidTracker = () => {
+  const [data, setData] = useState([]);
+
+  const getCovidData = async () => {
+    const res = await fetch("https://api.covid19india.org/data.json");
+    const actualData = await res.json();
+    setData(actualData.statewise);
+  };
+
+  useEffect(() => {
+    getCovidData();
+  }, []);
+  return (
+    <>
+      <div className="contanier-fluid mt-5">
+        <div className="main-heading">
+          <h1 className="mb-5 text-center">
+            <span className="font-weight-bold">India</span>Covid-19 Dashboard
+          </h1>
+        </div>
+        <div className="table-responsive">
+          <table className="table-hover">
+            <thead className="thead-dark">
+              <tr>
+                <th>State</th>
+                <th>Confirmed</th>
+                <th>Recovered</th>
+                <th>Death</th>
+                <th>Active</th>
+                <th>Updated</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((curElem, ind) => {
+                return (
+                  <tr key={ind}>
+                    <td>{curElem.state}</td>
+                    <td>{curElem.confirmed}</td>
+                    <td>{curElem.recovered}</td>
+                    <td>{curElem.deaths}</td>
+                    <td>{curElem.active}</td>
+                    <td>{curElem.lastupdatedtime}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CovidTracker;
